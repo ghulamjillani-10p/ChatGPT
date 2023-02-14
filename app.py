598637096dc3ab80a364de2360d56f8dfd6959c9
ghulamjillani-10p <ghulam.jillani@10pearls.com>
2023-02-15 00:42:33 +0500

    Added text file feature

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, request
 import openai
 import webbrowser
-import requests
 
 app = Flask(__name__)
-API_KEY = # Add your API key here
+API_KEY = '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'
 openai.api_key = API_KEY
 model = 'text-davinci-003'
+file_content = None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global file_content
     if request.method == 'POST':
         prompt = request.form['prompt']
-        response = openai.Completion.create(
-            prompt=prompt,
-            model=model, 
-            max_tokens=2500
-        )
-        res = response.choices[0].text
-        htmlstring = res
-        with open("code.html", "w") as file:
-            file.write(htmlstring)
-            
-        generated_code = res
+        uploaded_file = request.files['file']
+        if uploaded_file:
+            file_content = uploaded_file.read()
+        if file_content:
+            prompt1 = prompt + '\n' + file_content.decode('utf-8')
+            response = openai.Completion.create(
+                prompt=prompt1,
+                model=model, 
+                max_tokens=2500
+            )
+            res = response.choices[0].text
+            htmlstring = res
+            with open("code.html", "w") as file:
+                file.write(htmlstring)
+            #webbrowser.open("code.html")
+            return render_template('index.html', response=res, prompt=prompt)
 
-        webbrowser.open("code.html")
-        return render_template('index.html', response=res)
     return render_template('index.html')
 
 if __name__ == '__main__':
